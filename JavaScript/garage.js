84354edf40451078a852109ab21464715bed7820
Devashish <devashish.bishoni@qa.com>
2017-08-24 15:02:35 +0100

    Finished garage part 1&2. Working on part 3

--- JavaScript/garage.js
+++ JavaScript/garage.js
@@ -1,17 +1,27 @@
   let garage = [];
+  let carList = [];
   
 
-  function checkIn() {
-      let car = {
-          name: "",
-          regNum: "",
-          ownNum: ""
-      };
+  function checkIn(g) {
+    let car = {
+        name: "",
+        regNum: "",
+        seats: "",
+        faults: null
+    };
       car.name = document.getElementById("name").value;
       car.regNum = document.getElementById("rnum").value;
-      car.ownNum = document.getElementById("onum").value;
+      car.seats = document.getElementById("onum").value;
+      car.faults = document.getElementById("faults").value;
+      if(g===1){
       garage.push(car);
-      alert("checked in!");
+      alert("checked in garage");
+      }
+      else {
+          carList.push(car);
+          alert("checked in car list!");
+      }
+     
   }
 
   function checkOut() {
@@ -26,23 +36,58 @@
       }
   }
 
-  function show() {
+  function show(state) {
 
       let ul = document.createElement('ul');
       document.getElementById('mypara').appendChild(ul);
-      for (let i = 0; i <= garage.length; i++) {
+      
+      for (let i = 0; i <= state.length; i++) {
           let li = document.createElement('li');
-          li.appendChild(document.createTextNode("Name: " + garage[i].name));
+         
+          li.appendChild(document.createTextNode(" Name: " + state[i].name));
           ul.appendChild(li);
-          li.appendChild(document.createTextNode(" Registration number: " + garage[i].regNum));
+          li.appendChild(document.createTextNode(" Registration number: " + state[i].regNum));
           ul.appendChild(li);
-          li.appendChild(document.createTextNode(" Owner number: " + garage[i].ownNum));
+          li.appendChild(document.createTextNode(" Seat number: " + state[i].seats));
           ul.appendChild(li);
-
-
+          li.appendChild(document.createTextNode(" Faults: " + state[i].faults));
+          ul.appendChild(li);
       }
   }
 
   function calculate() {
+      let r = document.getElementById("carreg").value;
+      let print = 0;
+      for (let i = 0; i <= garage.length; i++) {
+
+          if (garage[i].regNum === r) {
+              console.log("inside");
+              num = garage[i].seats;
+              let price = num * 50;
+              document.getElementById("price").innerHTML = price;
+              print = 1;
+          }
+
+      }
+      /*if(print===0){   Fix here!
+          alert("no car");
+         document.getElementById("price").innerHTML("Car not found!");
+      }*/
 
   }
+  function toGarage(){
+   
+    checkIn(1);
+  }
+  function toCar(){
+    
+    checkIn(0);
+  }
+  function showGarage(){
+    document.getElementById("mypara").innerHTML = "Showing Garage";
+     show(garage);
+   }
+   function showCar(){
+    document.getElementById("mypara").innerHTML ="Showing Car";
+     show(carList);
+   }
